9637088a4f2c9b82279867f13858b0b61b705550
Anish Karthik <89824626+anishfish2@users.noreply.github.com>
2021-10-11 22:22:44 -0500

    Update Model.py
    Updated Comments

--- Model.py
+++ Model.py
@@ -1,3 +1,4 @@
+#Import statements - Using Tensorflow
 import numpy as np 
 import pandas as pd 
 import os
@@ -14,6 +15,8 @@ from keras.applications.resnet import ResNet50
 from keras.optimizers import Adam
 from keras.callbacks import ModelCheckpoint, LearningRateScheduler, EarlyStopping, ReduceLROnPlateau
 from tensorflow.keras.models import Sequential, save_model, load_model
+
+#Dataframe using Kaggle Dataset
 df = pd.read_csv('C:/Users/anish_8d0sbo2/Hackathon5/ISIC_2019_Training_GroundTruth.csv')
 list1 = []
 for index, row in df.iterrows():
@@ -22,6 +25,7 @@ for index, row in df.iterrows():
             list1.append(i)
 df['class'] = list1
 
+#Remove excess Labels
 del df['MEL']
 del df['NV']
 del df['BCC']
@@ -32,9 +36,11 @@ del df['VASC']
 del df['SCC']
 del df['UNK']
 
+#Seperature train, validation test sets
 print(df.head(50))
 train_df, valid_df = skl.train_test_split(df, test_size = 0.2,)
 
+#image modification through generators
 train_idg = ImageDataGenerator(rescale=1. / 255.0,horizontal_flip = True, vertical_flip = False, height_shift_range= 0.1, width_shift_range=0.1, 
 rotation_range=20, shear_range = 0.1,zoom_range=0.1)
 
@@ -46,6 +52,8 @@ val_idg = ImageDataGenerator(rescale=1. / 255.0)
 val_gen = val_idg.flow_from_dataframe(dataframe=valid_df,directory='C:/Users/anish_8d0sbo2/Hackathon5/ISIC_2019_Training_Input/ISIC_2019_Training_Input', x_col = 'img_path',y_col = 'class',class_mode = 'categorical',target_size = (224, 224), 
 batch_size = 6)
 valX, valY = val_gen.next()
+
+#plot images
 fig, m_axs = plt.subplots(5,4, figsize = (16, 16))
 m_axs = m_axs.flatten()
 imgs = 'C:/Users/anish_8d0sbo2/Hackathon5/ISIC_2019_Training_Input/ISIC_2019_Training_Input/'+ df.image + '.jpg'
@@ -57,11 +65,17 @@ for img, ax in zip(imgs, m_axs):
     ax.set_title(df.iloc[ind]['class'])
     ind=ind+1
 
+#Utilize pretrained VGG16 image model
 model = VGG16(include_top=True, weights='imagenet')
 transfer_layer = model.get_layer('block5_pool')
 vgg_model = Model(inputs=model.input,outputs=transfer_layer.output)
+
+#Establish unchangable layers
 for layer in vgg_model.layers[0:17]:
     layer.trainable = False
+    
+    
+#Adding layers to pretrained model + dropout for overfitting
 new_model = Sequential()
 new_model.add(vgg_model)
 new_model.add(Flatten())
@@ -70,11 +84,17 @@ new_model.add(Dropout(0.2))
 new_model.add(Dense(512, activation='relu'))
 new_model.add(Dropout(0.2))
 new_model.add(Dense(8, activation='sigmoid'))
+
+#Determine optimizer, loss, performance metric
 optimizer = Adam(lr=1e-5)
 loss = 'CategoricalCrossentropy'
 metrics = ['binary_accuracy']
 print(new_model.summary())
+
+#Compile model + establish history, filepath
 new_model.compile(optimizer=optimizer, loss=loss, metrics=metrics)
 history = new_model.fit_generator(train_gen, validation_data = (valX, valY), epochs = 5, steps_per_epoch=16)
 filepath = './saved_model'
+
+#Save model
 save_model(model, filepath, overwrite=True)
